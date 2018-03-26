16cff3b333a0d3daf0e1fc35a8149aa81ef24c68
aeshwer.tyagi <aeshwer.tyagi@iongroup.com>
2018-03-26 18:22:46 +0530

    NEW

--- NodeJsProject/demoModule.js
+++ NodeJsProject/demoModule.js
@@ -1,8 +1,10 @@
 var http = require('http');
 var dt = require('./myFirstModule');
 
+
+//create a server object:
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-	 res.write("The date and time are currently: " + dt.myDateTime());
-    res.end('Hello World!');
-}).listen(8085);
+    //res.writeHead(200, {'Content-Type': 'text/html'});
+	 res.write("The date and time are currently: " + dt.myDateTime());	//write a response to the client
+    res.end('Hello World!');	//end the response
+}).listen(8085);  //the server object listens on port 8085
