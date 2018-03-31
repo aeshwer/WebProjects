90cffc9f9124f29b30ccd5a1f90e3fabb9fc54b2
Aeshwer Tyagi <aeshwer.tyagi16@gmail.com>
2018-03-31 07:05:25 -0700

    java script

---
+++ JavaScriptLearn/PigGame/appTest.js
@@ -0,0 +1,98 @@
+
+var scores, roundScore, activePlayer, gamePlaying;
+
+init();
+
+//document.querySelector("#current-" + activePlayer).textContent= dice;
+
+//document.querySelector("#current-" + activePlayer).innerHTML= '<em>' + dice + '</em>';
+
+
+// ROLL EVENT
+document.querySelector('.btn-roll').addEventListener('click',function()
+{  
+    var dice =  Math.floor(Math.random()*6)+1;
+    console.log(dice);
+    
+    var x = document.querySelectorAll('.dice');
+    var i;
+  
+    for(i=0;i<x.length;i++)
+    {    
+        
+    x[i].style.display = 'block';     
+    x[i].src = 'dice-'+ dice +'.png'
+        
+    }
+    
+    if(dice !== 1)
+        {
+          roundScore +=dice;
+          document.querySelector('#current-'+activePlayer).textContent=roundScore;
+            
+        }
+    else
+    {
+    nextPlayer();
+    }    
+});
+
+
+
+// HOLD EVENT
+document.querySelector('.btn-hold').addEventListener('click',function(){
+    
+    scores[activePlayer] += roundScore;
+    document.querySelector('#score-'+ activePlayer).textContent = scores[activePlayer];
+             
+    if(scores[activePlayer]>=20)
+        {
+            document.querySelector('#name-'+ activePlayer).textContent = 'Winner';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            
+        }
+    else
+    {
+        nextPlayer();
+    }
+});
+
+
+
+function nextPlayer()
+{   
+     //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+function init()
+{    
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+    
+     document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+}
