51a14731fb206da41e2344382c26cd686cdc007f
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2019-05-17 16:23:06 +0530

    tip project

--- JavaScriptProjects/TipCalculator/app.js
+++ JavaScriptProjects/TipCalculator/app.js
@@ -4,13 +4,45 @@ var userBillAmount = document.getElementById("userBillAmount");
 var selectedServiceType = document.getElementById("selectedServiceType");
 var noOfPeople = document.getElementById("noOfPeople");
 var calculateButton = document.getElementById("calculateButton");
-        
-
-function inputLength(){
-	return userInput.value.length;
+var clearButton = document.getElementById("clearButton");
+    
+function inputLength(input){   
+	return input.value.length;
 } 
+    
+function resolveTip(){
+    if(inputLength(noOfPeople)<=0){
+        noOfPeople.value=1;
+    }
+    return (userBillAmount.value * selectedServiceType.value)/noOfPeople.value;
+} 
+    
+function createTipElement(tip){
+    var element = document.getElementById("calculatedTip");
+    var content = document.createTextNode("Calculated Tip Per-Person:" +tip);
+    element.appendChild(content);
+} 
+        
+function calculateTip(){   
+        if(inputLength(userBillAmount)<=0 || inputLength(selectedServiceType)<=0 ){
+            alert('Please Enter Details for tip calculation');
+        }
+        else{            
+            var tip = resolveTip();
+            console.log(tip);
+            createTipElement(tip)
+        }
+}
+    
+function clearInputFields(){   
+        userBillAmount.value ='';
+        selectedServiceType.value ='';
+        noOfPeople.value ='';
+        var elem = document.getElementById("calculatedTip");
+        elem.parentNode.removeChild(elem);
+}
 
-//Add events
-inputButton.addEventListener("click",addListAfterClick);
-userInput.addEventListener("keypress", addListAfterKeypress);
+//Adding events
+calculateButton.addEventListener("click",calculateTip);
+clearButton.addEventListener("click",clearInputFields);
 }());
