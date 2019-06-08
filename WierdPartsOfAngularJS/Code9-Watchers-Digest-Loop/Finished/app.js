7199a49b21a51db93125d98090463e0bc766c2ba
aeshwer <aeshwer.tyagi16@gmail.com>
2019-06-08 05:09:03 -0400

    update

---
+++ WierdPartsOfAngularJS/Code9-Watchers-Digest-Loop/Finished/app.js
@@ -0,0 +1,36 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', '$filter', '$timeout', function($scope, $filter, $timeout) {
+    
+    $scope.handle = '';
+    
+    $scope.lowercasehandle = function() {
+        return $filter('lowercase')($scope.handle);
+    };
+    
+    $scope.$watch('handle', function(newValue, oldValue) {
+        
+        console.info('Changed!');
+        console.log('Old:' + oldValue);
+        console.log('New:' + newValue);
+        
+    });
+    
+    $timeout(function() {
+       
+        $scope.handle = 'newtwitterhandle';
+        console.log('Scope changed!');
+        
+    }, 3000);
+    
+    setTimeout(function() {
+       
+        $scope.$apply(function(){
+        $scope.handle = 'newtwitterhandle2';
+        console.log('Scope changed!');
+        });
+        
+    }, 6000);
+    
+    
+}]);
