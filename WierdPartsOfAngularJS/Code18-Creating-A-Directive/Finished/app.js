98e5846607ac49dc6adc500608676f820ebab4d6
Aeshwer Tyagi <aeshwer.tyagi@iongroup.com>
2019-06-14 15:21:19 +0530

    updatess

---
+++ WierdPartsOfAngularJS/Code18-Creating-A-Directive/Finished/app.js
@@ -0,0 +1,45 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+    
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+    
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+    
+    .when('/second/:num', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+    
+});
+
+myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+    
+    
+    
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
+    
+    
+    
+}]);
+
+myApp.directive("searchResult", function() {
+   return {
+       restrict: 'AECM',
+       template: '<a href="#" class="list-group-item"><h4 class="list-group-item-heading">Doe, John</h4><p class="list-group-item-text">555 Main St., New York, NY 11111</p></a>',
+       replace: true
+   }
+});
+
+
+//Note: A-attribute, E-Element ,C-class,M=Comment  , INcude these by letter in 'restrict' parameter to be used that way.See four ways to use these in main.html
